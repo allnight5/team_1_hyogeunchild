9e86fc3498b6c068aa59492df33f38d7e155aa93
allnight5 <deathnight1579@gmail.com>
2023-01-18 09:30:37 +0900

    enum을 활용한 메시지 작성
    enum에 메시지 작성해서 넣어줄수있는 enum클래스 추가
    service부분 message수정

--- src/main/java/com/sparta/team_1_hyogeunchild/business/service/AdminService.java
+++ src/main/java/com/sparta/team_1_hyogeunchild/business/service/AdminService.java
@@ -1,6 +1,7 @@
 package com.sparta.team_1_hyogeunchild.business.service;
 
 import com.sparta.team_1_hyogeunchild.business.dto.PromoteAdminResponseDto;
+import com.sparta.team_1_hyogeunchild.enums.MessageEnum;
 import com.sparta.team_1_hyogeunchild.enums.UserRoleEnum;
 import com.sparta.team_1_hyogeunchild.persistence.entity.Promote;
 import com.sparta.team_1_hyogeunchild.persistence.entity.User;
@@ -21,15 +22,13 @@ public class AdminService {
     //1.구매자 -> 판매자로 승급
     @Transactional
     public PromoteAdminResponseDto promoteBuyer(String username){
-        String message = "판매자로의 등급 변환에 성공했습니다.";
-        return getPromoteResponseDto(username, UserRoleEnum.SELLER, message);
+        return getPromoteResponseDto(username, UserRoleEnum.SELLER);
     }
     //2. 판매자 자격 박탈->구매자
     //buyerAuthorization or promoteLossOfAuthority
     @Transactional
     public PromoteAdminResponseDto degradeSeller(String username){
-        String message ="구매자로 등급 변환에 성공했습니다.";
-        return getPromoteResponseDto(username, UserRoleEnum.BUYER, message);
+        return getPromoteResponseDto(username, UserRoleEnum.BUYER);
     }
     //3. 유저 목록 조회
 
@@ -38,7 +37,7 @@ public class AdminService {
     //5. 등급 업 심사 대기중인 사람들 조회
 
     //6. (4번과 5번 통합)구매자 -> 판매자로 승급, 판매자 자격 박탈->구매자
-    private PromoteAdminResponseDto getPromoteResponseDto(String username, UserRoleEnum seller, String message) {
+    private PromoteAdminResponseDto getPromoteResponseDto(String username, UserRoleEnum seller) {
         User user = userRepository.findByUsername(username).orElseThrow(
                 ()-> new IllegalArgumentException("사용자를 찾을수 없습니다.")
         );
@@ -50,10 +49,10 @@ public class AdminService {
                 );
                 promoteRepository.delete(promote);
                 user.promote(promote.getStoreName(), seller);
-                return new PromoteAdminResponseDto(message);
+                return new PromoteAdminResponseDto(MessageEnum.PROMOTE_BUYER.getMessage());
             }
             user.promote(null, seller);
-            return new PromoteAdminResponseDto(message);
+            return new PromoteAdminResponseDto(MessageEnum.DEGRADE_SELLER.getMessage());
         }
         throw new SecurityException("관리자의 권한을 변경할수없습니다..");
     }
--- src/main/java/com/sparta/team_1_hyogeunchild/enums/MessageEnum.java
+++ src/main/java/com/sparta/team_1_hyogeunchild/enums/MessageEnum.java
@@ -1,7 +1,16 @@
 package com.sparta.team_1_hyogeunchild.enums;
 
+
+import lombok.Getter;
+
+@Getter
 public enum MessageEnum {
 
-
+    PROMOTE_BUYER("판매자로의 등급 변환에 성공했습니다."),
+    DEGRADE_SELLER("구매자로 등급 변환에 성공했습니다.");
+    private final String message;
+    MessageEnum(String message){
+        this.message = message;
+    }
 
 }
